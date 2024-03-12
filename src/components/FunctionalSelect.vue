a22e0f67b3fe19a0a949c0d6e239a405c82032d4
checkma_xh <checkma_xh@outlook.com>
2024-03-12 11:06:55 +0800

    nothing

---
+++ src/components/FunctionalSelect.vue
@@ -0,0 +1,24 @@
+<template>
+	<ion-select
+		:label="label"
+		:placeholder="placeholder"
+		:value="selectValue"
+		@ionChange="handleChange">
+		<ion-select-option
+			v-for="(value, index) of selectOptions"
+			:key="index"
+			:value="value"
+			>{{ value }}
+		</ion-select-option>
+	</ion-select>
+</template>
+
+<script setup lang="ts">
+import { IonSelect, IonSelectOption } from "@ionic/vue";
+
+const label = defineModel("label");
+const placeholder = defineModel("placeholder");
+const selectValue = defineModel("selectValue");
+const handleChange = defineModel("handleChange");
+const selectOptions = defineModel("selectOptions");
+</script>
