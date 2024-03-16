e2d09673174d746db094f48afa403e24817d4cdd
checkma_xh <checkma_xh@outlook.com>
2024-03-16 21:33:21 +0800

    剩余任务部署反向代理服务器，完善主题，完成回收站功能，添加缓存数据库提高性能

--- database/src/utils/ConfigService.ts
+++ database/src/utils/ConfigService.ts
@@ -11,6 +11,12 @@ export interface Config {
     verificationCodeLen   : number;
     verificationMaxCount  : number;
     defaultAvatar         : string,
+    databaseHostUserName  : string,
+    databaseHost          : string,
+    databasePort          : number,
+    databasePassword      : string,
+    databaseType          : string,
+    databaseName          : string,
 }
 
 export class ConfigService {
@@ -27,6 +33,12 @@ export class ConfigService {
                 defaultAvatar         : process.env.DEFAULT_AVATAR || "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGNsYXNzPSJpb25pY29uIiB2aWV3Qm94PSIwIDAgNTEyIDUxMiI+PHBhdGggZD0iTTI1OC45IDQ4QzE0MS45MiA0Ni40MiA0Ni40MiAxNDEuOTIgNDggMjU4LjljMS41NiAxMTIuMTkgOTIuOTEgMjAzLjU0IDIwNS4xIDIwNS4xIDExNyAxLjYgMjEyLjQ4LTkzLjkgMjEwLjg4LTIxMC44OEM0NjIuNDQgMTQwLjkxIDM3MS4wOSA0OS41NiAyNTguOSA0OHptMTI2LjQyIDMyNy4yNWE0IDQgMCAwMS02LjE0LS4zMiAxMjQuMjcgMTI0LjI3IDAgMDAtMzIuMzUtMjkuNTlDMzIxLjM3IDMyOSAyODkuMTEgMzIwIDI1NiAzMjBzLTY1LjM3IDktOTAuODMgMjUuMzRhMTI0LjI0IDEyNC4yNCAwIDAwLTMyLjM1IDI5LjU4IDQgNCAwIDAxLTYuMTQuMzJBMTc1LjMyIDE3NS4zMiAwIDAxODAgMjU5Yy0xLjYzLTk3LjMxIDc4LjIyLTE3OC43NiAxNzUuNTctMTc5UzQzMiAxNTguODEgNDMyIDI1NmExNzUuMzIgMTc1LjMyIDAgMDEtNDYuNjggMTE5LjI1eiIvPjxwYXRoIGQ9Ik0yNTYgMTQ0Yy0xOS43MiAwLTM3LjU1IDcuMzktNTAuMjIgMjAuODJzLTE5IDMyLTE3LjU3IDUxLjkzQzE5MS4xMSAyNTYgMjIxLjUyIDI4OCAyNTYgMjg4czY0LjgzLTMyIDY3Ljc5LTcxLjI0YzEuNDgtMTkuNzQtNC44LTM4LjE0LTE3LjY4LTUxLjgyQzI5My4zOSAxNTEuNDQgMjc1LjU5IDE0NCAyNTYgMTQ0eiIvPjwvc3ZnPg==",
                 verificationCodeLen   : parseInt(process.env.VERIFICATION_CODE_LEN) || 4,
                 verificationMaxCount  : parseInt(process.env.VERIFICATION_MAX_COUNT) || 5,
+                databaseHostUserName  : process.env.DATABASE_HOST_USER_NAME || "root",
+                databaseHost          : process.env.DATABASE_HOST || "localhost",
+                databasePort          : parseInt(process.env.DATABASE_PORT) || 0,
+                databasePassword      : process.env.DATABASE_PASSWORD || "",
+                databaseType          : process.env.DATABASE_TYPE || "",
+                databaseName          : process.env.DATABASE_NAME || "",
             };
         }
         return ConfigService.config;
